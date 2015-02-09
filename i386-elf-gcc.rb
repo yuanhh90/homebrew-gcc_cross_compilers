f76ea899482a6e5afac35fafa5549e8c3cbd9438
Li Qi <chrislitex@gmail.com>
2015-02-09 13:47:43 +0800

    use gcc49 in homebrew instead of apple-gcc42

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
+  revision 1
 
   depends_on 'gmp'
   depends_on 'libmpc'
@@ -14,10 +15,10 @@ class I386ElfGcc < Formula
   def install
     binutils = Formula.factory 'i386-elf-binutils'
 
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['CC'] = '/usr/local/bin/gcc-4.9'
+    ENV['CXX'] = '/usr/local/bin/g++-4.9'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.9'
+    ENV['LD'] = '/usr/local/bin/gcc-4.9'
     ENV['PATH'] += ":#{binutils.prefix/"bin"}"
 
     mkdir 'build' do
--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -1,20 +1,25 @@
 require 'formula'
 
 class I386ElfGdb < Formula
-  homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.6.tar.bz2'
-  sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
+  homepage 'http://www.gnu.org/software/gdb/'
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.xz'
+  md5 'a80cf252ed2e775d4e4533341bbf2459'
 
   depends_on 'i386-elf-binutils'
   depends_on 'i386-elf-gcc'
 
   def install
+    # ENV['CC'] = '/usr/local/bin/gcc-4.9'
+    # ENV['CXX'] = '/usr/local/bin/g++-4.9'
+    # ENV['CPP'] = '/usr/local/bin/cpp-4.9'
+    # ENV['LD'] = '/usr/local/bin/gcc-4.9'
+
     mkdir 'build' do
       system '../configure', '--target=i386-elf', "--prefix=#{prefix}", '--disable-werror'
       system 'make'
       system 'make install'
-      FileUtils.rm_rf share/"locale"
-      FileUtils.mv lib, libexec
+      #FileUtils.rm_rf share/"locale"
+      #FileUtils.mv lib, libexec
     end
   end
 end
