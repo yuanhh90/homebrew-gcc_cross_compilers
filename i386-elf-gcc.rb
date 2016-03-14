e9fe4e4b2762fcf19051fbeeb9b27fda435d473e
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2016-03-13 20:58:57 -0400

    Update i386-elf-gcc to use gcc 4.9.3

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.3/gcc-4.9.3.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.3/gcc-4.9.3.tar.bz2'
   sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
   revision 1
 
