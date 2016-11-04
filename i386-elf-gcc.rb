7ad6b29715283958b21ab704ef24c886376febe2
Yuki Izumi <kivikakk@github.com>
2016-11-04 15:59:40 +1100

    Change from sha1 to sha256
    Avoids deprecation warning from homebrew

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -4,7 +4,7 @@ class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.3/gcc-4.9.3.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.3/gcc-4.9.3.tar.bz2'
-  sha1 '2e6490a908eac8b2b56b73679bd7c2a5237df1f8'
+  sha256 '1934f3f68fe477299fe929e527c62d800f8b1b01ca74cc218f90459715ace296'
   revision 1
 
   depends_on 'gmp'
--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class I386ElfGdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
   url 'http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.xz'
-  sha1 '85a9cc2a4dfb748bc8eb74113af278524126a9bd'
+  sha256 '605954d5747d5f08ea4b7f48e958d1ebbf39265e18f7f36738deeabb83744485'
 
   depends_on 'i386-elf-binutils'
   depends_on 'i386-elf-gcc'
