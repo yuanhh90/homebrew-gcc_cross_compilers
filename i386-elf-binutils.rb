a9f3bf721441dbe154bddc4a4569c7db2f1fc007
朱骏 <zhujun@zhujunMac.local>
2017-05-16 21:11:32 +0800

    modify version forfix version conflict

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
   sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
 
-  depends_on 'gcc49' => :build
+  depends_on 'gcc52' => :build
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.9'
-    ENV['CXX'] = '/usr/local/bin/g++-4.9'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.9'
-    ENV['LD'] = '/usr/local/bin/gcc-4.9'
+    ENV['CC'] = '/opt/local/gcc-5.2'
+    ENV['CXX'] = '/opt/local/g++-5.2'
+    ENV['CPP'] = '/opt/local/cpp-5.2'
+    ENV['LD'] = '/opt/local/gcc-5.2'
 
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=i386-elf',
